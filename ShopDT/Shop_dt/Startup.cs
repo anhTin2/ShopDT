488c38850b3e4677d868d18381b6f193e68b9d55
anhTin2 <tin27072001@gmail.com>
2021-04-04 14:32:34 +0700

    upload Project first time

---
+++ ShopDT/Shop_dt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Shop_dt.Startup))]
+namespace Shop_dt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
